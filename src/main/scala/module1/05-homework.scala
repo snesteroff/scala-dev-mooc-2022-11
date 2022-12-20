a045f322136811f554f2618fa791cbb533f2e52f
snesteroff <stanislav.nesterov@gmail.com>
2022-12-20 15:54:10 +0300

    Временный комит

--- src/main/scala/module1/05-homework.scala
+++ src/main/scala/module1/05-homework.scala
@@ -3,16 +3,17 @@ package module1
 import scala.util.Random
 
 class BallsExperiment {
+  val blackOrWhite = Random.nextBoolean()
 
-  def isFirstBlackSecondWhite(): Boolean = {
-    ???
-  }
+  def isFirstBlackSecondWhite(): Boolean = blackOrWhite
 }
 
 object BallTest {
   def main(args: Array[String]): Unit = {
     val count = 10000
-    val listOfExperiments: List[BallsExperiment] = ???
+    val listOfExperiments: List[BallsExperiment] = List.fill(count) {
+
+    }
     val countOfExperiments = ???
     val countOfPositiveExperiments: Float = countOfExperiments.count(_ == true)
     println(countOfPositiveExperiments / count)

d02e521f090ceef779627126b4f07f5e05de6086
Stanislav Nesterov <stanislav.nesterov@gmail.com>
2022-12-25 18:54:48 +0300

    Проверочное задание № 2

--- src/main/scala/module1/05-homework.scala
+++ src/main/scala/module1/05-homework.scala
@@ -3,18 +3,25 @@ package module1
 import scala.util.Random
 
 class BallsExperiment {
-  val blackOrWhite = Random.nextBoolean()
-
-  def isFirstBlackSecondWhite(): Boolean = blackOrWhite
+  val binOfBalls: Vector[Boolean] = Vector(true, true, true, false, false, false)
+  val sizeOfBin = binOfBalls.size
+  def isFirstBlackSecondWhite(): Boolean = {
+    val i = Random.nextInt(sizeOfBin)
+    var j: Int = -1
+    do {
+      j = Random.nextInt(sizeOfBin)
+    } while (j == i)
+    if (!binOfBalls(i) && binOfBalls(j)) true else false
+  }
 }
 
 object BallTest {
   def main(args: Array[String]): Unit = {
-    val count = 10000
+    val count = 100000
     val listOfExperiments: List[BallsExperiment] = List.fill(count) {
-
+      new BallsExperiment
     }
-    val countOfExperiments = ???
+    val countOfExperiments = listOfExperiments.map(_.isFirstBlackSecondWhite())
     val countOfPositiveExperiments: Float = countOfExperiments.count(_ == true)
     println(countOfPositiveExperiments / count)
   }

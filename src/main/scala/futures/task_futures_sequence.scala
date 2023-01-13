e7fb01e2f1e2f3c84d55a99e9c004bf40f4f9fff
snesteroff <stanislav.nesterov@gmail.com>
2023-01-13 18:01:45 +0300

    Временный комит

--- src/main/scala/futures/task_futures_sequence.scala
+++ src/main/scala/futures/task_futures_sequence.scala
@@ -2,7 +2,11 @@ package futures
 
 import HomeworksUtils.TaskSyntax
 
-import scala.concurrent.{ExecutionContext, Future}
+
+import scala.::
+import scala.concurrent.duration.{Duration, DurationInt}
+import scala.concurrent.{Await, ExecutionContext, Future}
+import scala.util.{Failure, Success}
 
 object task_futures_sequence {
 
@@ -15,11 +19,48 @@ object task_futures_sequence {
    * в правово результаты неуспешных выполнений.
    * Не допускается использование методов объекта Await и мутабельных переменных var
    */
+
+  def main(arg: Array[String]) = {
+    import scala.concurrent.ExecutionContext.Implicits.global
+    val li = Future.successful(5)
+
+    val ex1 = new Exception("ex1")
+    val ex2 = new Exception("ex2")
+    val failed1 = Future.failed(ex1)
+    val failed2 = Future.failed(ex2)
+
+    // , Future.failed(new Exception())
+    val f = fullSequence(List(Future {
+      new RuntimeException()
+    }, li, Future.successful(1),
+      Future {
+        new NullPointerException()
+      },
+      Future.successful(2), Future {
+        Thread.sleep(1000); 6
+      },
+      failed2, failed1
+    ))
+
+    f.foreach { x =>
+      println(x)
+    }
+    Thread.sleep(3000)
+  }
+
   /**
    * @param futures список асинхронных задач
    * @return асинхронную задачу с кортежом из двух списков
    */
   def fullSequence[A](futures: List[Future[A]])
-                     (implicit ex: ExecutionContext): Future[(List[A], List[Throwable])] =
-    task"Реализуйте метод `fullSequence`"()
+                     (implicit ex: ExecutionContext): Future[(List[A], List[Throwable])] = {
+    //  def fuseq[A](f: List[Future[A]])(implicit ex: ExecutionContext): Future[(List[A], List[Throwable])] = {
+    val ff = futures.foldLeft(Future.successful((List.empty[A], List.empty[Throwable]))) {
+      (fr, fa) =>
+        fr.zipWith(fa)((t, a) => if (a.isInstanceOf[Throwable])
+          (t._1, t._2 :+ a.asInstanceOf[Throwable]) else
+          (t._1 :+ a, t._2))
+    }
+    ff.map(identity)(ex)
+  }
 }
